45da07b41b863639f4b46a7465fdbe5489f96804
Yeva-Yar <yeva.yaroshenko@gmail.com>
2025-05-04 15:38:53 +0300

    dfghjk

---
+++ db.js
@@ -0,0 +1,70 @@
+let mysql = require("mysql2")
+require("dotenv").config()
+
+let db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS
+})
+
+let adb = db.promise()
+
+async function getUsers(content, author_id){
+    try{
+        let [users, flieds] = await adb.query("SELECT * FROM User")
+        return users
+    }catch(err){
+        throw err.message
+    }
+}
+
+async function addMessage(content, author_id){
+    try{
+        let [users, flieds] = await adb.query("insert into Message(content, author_id) values(?,?)" , [content,author_id])
+        return users
+    }catch(err){
+        throw err.message
+    }
+}
+
+async function getMessages(content, author_id){
+    try{
+        let [users, flieds] = await adb.query(`SELECT m.id, m.content, m.author_id, u.login
+            FROM Message as m
+            JOIN User as u
+            ON m.author_id = u.id`)
+        return users
+    }catch(err){
+        throw err.message
+    }
+}
+
+
+
+async function existUsers(login){
+    try{
+        let [users, flieds] = await adb.query("SELECT * FROM User WHERE login = ?", [login])
+        return users.length > 0
+    }catch(err){
+        throw err.message
+    }
+}
+
+async function addUsers(login, password){
+    try{
+        let [users, flieds] = await adb.query("INSENT INTO User(login, password)VALUES(?,?)", [login, password])
+        return users
+    }catch(err){
+        throw err.message
+    }
+}
+
+module.exports = {
+    getUsers,
+    getMessages,
+    addMessage,
+    existUsers,
+    addUsers
+}
---
+++ static/script.js
@@ -0,0 +1,33 @@
+const server = io()
+let nickname = "Sherlok"
+document.querySelector(".form button").addEventListener("click", sendMessage)
+
+function sendMessage(){
+    let input = document.querySelector(".form input").value 
+    document.querySelector(".form input").value = ""
+    server.emit("message", JSON.stringify({
+        user: nickname,
+        message: input
+    }))
+}
+
+server.on("update", (data)=>{
+    let chat = JSON.parse(data)
+    console.log(chat)
+    let main = document.querySelector("main")
+    main.innerHTML = ""
+    chat.forEach((message)=>{
+        main.innerHTML += `<div class="message">${message.user}: ${message.message}</div>`
+    })
+})
+
+
+
+alertify.success("Alertify is work!")
+
+
+document.querySelector("header button").addEventListener("click", ()=>{
+    alertify.prompt("Введіть свій нікнейм..", function(e, val){
+        if(e) nickname = val
+    })
+})
